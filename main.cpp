cfc160f418fea50d50fdaa2689ac77637913c8b7
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-06-24 21:03:45 +0300

    Argument

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if(argc < 3)
+    {
+        puts("Program recieves two arguments: DukeNukemMap TargetFileName.\n\0");
+        return 0;
+    }
     MapReader reader;
-    reader.open("./newboard.map");
-    reader.convert("./map.txt");
+    reader.open(argv[1]);
+    reader.convert(argv[2]);
     puts("Done.\0");
     return 0;
 }
